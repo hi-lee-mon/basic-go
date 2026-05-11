942b5bd83c26033031dce8b93d8d5d0fcf190f8d
hi-lee-mon <shunmel.09@gmail.com>
2026-05-12 01:18:58 +0900

    feat: アイテム更新機能を追加
    アイテムの更新機能を実装し、関連するDTO、サービス、リポジトリ、コントローラーを更新しました。また、エラーメッセージ用の定数を追加しました。

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -13,6 +13,7 @@ type IItemController interface {
 	FindAll(ctx *gin.Context)
 	FindById(ctx *gin.Context)
 	Create(ctx *gin.Context)
+	Update(ctx *gin.Context)
 }
 
 type ItemController struct {
@@ -30,7 +31,6 @@ func (c *ItemController) FindAll(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, gin.H{"data": items})
-	return
 }
 
 func (c *ItemController) FindById(ctx *gin.Context) {
@@ -63,4 +63,30 @@ func (c *ItemController) Create(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, gin.H{"data": newItem})
+}
+
+func (c *ItemController) Update(ctx *gin.Context) {
+	itemId, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		return
+	}
+
+	var input dto.UpdateItemInput
+	if err := ctx.ShouldBindBodyWithJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updateItem, err := c.service.Update(uint(itemId), input)
+
+	if err != nil {
+		if err.Error() == "Item not found" {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": updateItem})
 }
